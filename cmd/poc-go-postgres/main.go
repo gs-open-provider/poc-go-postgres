18007ecf5faa08683ac6cafa7146c6c07352845b
prashanth-gs <prashanth@gapstars.net>
2019-05-29 12:08:41 +0530

    db initialization added

--- cmd/poc-go-postgres/main.go
+++ cmd/poc-go-postgres/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/go-pg/pg"
 	"github.com/gs-open-provider/poc-go-postgres/internal/configs"
 	"github.com/gs-open-provider/poc-go-postgres/internal/logger"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -11,4 +13,15 @@ func main() {
 
 	// Initialize Logger across the application
 	logger.InitializeZapCustomLogger()
+
+	dbusername := viper.GetString("pgdb.username")
+	dbpassword := viper.GetString("pgdb.password")
+	database := viper.GetString("pgdb.database")
+
+	db := pg.Connect(&pg.Options{
+		User:     dbusername,
+		Password: dbpassword,
+		Database: database,
+	})
+	defer db.Close()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/gs-open-provider/poc-go-postgres
 go 1.12
 
 require (
+	github.com/go-pg/pg v8.0.4+incompatible
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/spf13/viper v1.4.0
 	go.uber.org/zap v1.10.0
+	mellium.im/sasl v0.2.1 // indirect
 )
